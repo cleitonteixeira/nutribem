2a118b98f1ff10a3c582ace174d45cc286735abb
Cleiton Santos <cleitonteixeirasantos@gmail.com>
2025-01-27 15:04:44 -0300

    Melhorias nas funções compatilhadas do sistema

---
+++ billings/api/database_query.py
@@ -0,0 +1,224 @@
+import oracledb
+
+# configuração user
+
+username = "teknisa"
+password = "teknisa"
+dsn = "host:1521/teknisa"
+def consultaOperador():
+    print("############")
+    try:
+        connection = oracledb.connect(user=username, password=password,
+                                    host="192.168.0.91", port=1521, service_name="orclpdb")
+        print("############")
+        print(connection.version)
+
+        cursor = connection.cursor()
+
+        cursor.execute(f"""
+            SELECT CDOPERADOR, NMOPERADOR FROM OPERADOR       
+        """)
+        rows = cursor.fetchall()
+        return rows
+    except oracledb.DatabaseError as e:
+        print (e)
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+def consultaFiliais():
+    print("############")
+    try:
+        connection = oracledb.connect(user=username, password=password,
+                                    host="192.168.0.91", port=1521, service_name="orclpdb")
+        print("############")
+        print(connection.version)
+
+        cursor = connection.cursor()
+
+        cursor.execute(f"""
+            SELECT CDFILIAL, NMFILIAL FROM FILIAL WHERE NMFILIAL NOT LIKE '%INATIVO%' AND NMFILIAL NOT LIKE '%INAT%'     
+        """)
+        rows = cursor.fetchall()
+        return rows
+    except oracledb.DatabaseError as e:
+        print (e)
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+            
+def consultaRc():
+    print("############")
+    try:
+        connection = oracledb.connect(user=username, password=password,
+                                    host="192.168.0.91", port=1521, service_name="orclpdb")
+        print("############")
+        print(connection.version)
+
+        cursor = connection.cursor()
+
+        cursor.execute(f"""
+            SELECT
+                s.CDFILSOLI,
+                s.CDFILLANCA,
+                s.NRSOLICMP,
+                s.DTSOLCMP,
+                o.CDOPERADOR,
+                s.QTITEMSOLIC,
+                m.DSJUSTSOLEXT,
+                (
+                    SELECT MAX(its.DTHRAPROVSOL)
+                    FROM ITEMSOLI its
+                    WHERE its.CDFILSOLI = s.CDFILSOLI
+                    AND its.CDFILLAN = s.CDFILLANCA
+                    AND its.NRSOLICMP = s.NRSOLICMP
+                ) AS "HORA",
+                CASE
+                    WHEN COUNT(i.CDOPERAPROV) = COUNT(i.CDPRODUTO) THEN 'APROVADA'
+                    ELSE 'REPROVADA'
+                END AS "STATUS"
+            FROM SOLICITA s
+            INNER JOIN FILIAL f ON f.CDFILIAL = s.CDFILSOLI
+            INNER JOIN OPERADOR o ON o.CDOPERADOR = s.CDOPERLANC
+            INNER JOIN MOVSOLEXT m ON m.CDFILSOLEXT = s.CDFILSOLI 
+            AND m.CDFILLANCEXT = s.CDFILLANCA 
+            AND m.NRSOLIMOVEXT = s.NRSOLICMP
+            INNER JOIN ITEMSOLI i ON i.CDFILSOLI = s.CDFILSOLI 
+            AND i.CDFILLAN = s.CDFILLANCA 
+            AND i.NRSOLICMP = s.NRSOLICMP
+            WHERE s.DTSOLCMP >= TO_DATE('01/01/2025', 'DD/MM/YYYY') 
+            AND s.IDSOLEXTRA = 'S'
+            GROUP BY
+                s.CDFILSOLI,
+                f.NMFILIAL,
+                s.CDFILLANCA,
+                s.NRSOLICMP,
+                s.DTSOLCMP,
+                o.CDOPERADOR,
+                s.QTITEMSOLIC,
+                m.DSJUSTSOLEXT
+            HAVING
+                COUNT(i.CDOPERAPROV) = COUNT(i.CDPRODUTO)
+            ORDER BY s.DTSOLCMP, s.CDFILSOLI, s.NRSOLICMP
+        """)
+        rows = cursor.fetchall()
+        return rows
+    except oracledb.DatabaseError as e:
+        print (e)
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+def consultaItensRc(rc, solicita, destino):
+    print("############")
+    try:
+        connection = oracledb.connect(user=username, password=password,
+                                    host="192.168.0.91", port=1521, service_name="orclpdb")
+        print("############")
+        print(connection.version)
+
+        cursor = connection.cursor()
+
+        cursor.execute(f"""
+            SELECT NRSOLICMP,CDPRODUTO, QTSOLIPROD, DTUTILIZA FROM ITEMSOLI WHERE
+            CDFILSOLI = '{solicita.code}' AND CDFILLAN = '{destino.code}' AND NRSOLICMP = '{rc}'            
+        """)
+        rows = cursor.fetchall()
+        return rows
+    except oracledb.DatabaseError as e:
+        print (e)
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+def CriaClasse():
+    print("############")
+    try:
+        connection = oracledb.connect(user=username, password=password,
+                                    host="192.168.0.91", port=1521, service_name="orclpdb")
+        print("############")
+        print(connection.version)
+
+        cursor = connection.cursor()
+
+        cursor.execute(f"""
+            SELECT CDPRODUTO, NMPRODUTO FROM PRODUTO
+            WHERE NMPRODUTO NOT LIKE '.' HAVING LENGTH(CDPRODUTO) = 3 GROUP BY CDPRODUTO,NMPRODUTO ORDER BY CDPRODUTO
+        """)
+        rows = cursor.fetchall()
+        return rows
+    except oracledb.DatabaseError as e:
+        print (e)
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+def CriaProdutos():
+    print("############")
+    try:
+        connection = oracledb.connect(user=username, password=password,
+                                    host="192.168.0.91", port=1521, service_name="orclpdb")
+        print("############")
+        print(connection.version)
+
+        cursor = connection.cursor()
+
+        cursor.execute(f"""
+            SELECT 
+            CASE
+            WHEN SUBSTR(CDPRODUTO, 1, 3) = '866' THEN SUBSTR(CDPRODUTO, 1, 3)
+            ELSE SUBSTR(CDPRODUTO, 1, 1)
+            END
+            AS "CLASSE",CDPRODUTO, NMPRODUTO, SGUNIDADE FROM PRODUTO
+            WHERE NMPRODUTO NOT LIKE '.' AND CDPRODUTO IN
+            (SELECT DISTINCT(CDPRODUTO) FROM ITEMSOLI WHERE DTUTILIZA >= '01/01/2024' AND CDOPERAPROV IS NOT NULL)
+            ORDER BY NMPRODUTO
+        """)
+        rows = cursor.fetchall()
+        return rows
+    except oracledb.DatabaseError as e:
+        print (e)
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+def consultaProdutos(code):
+    print("############")
+    try:
+        connection = oracledb.connect(user=username, password=password,
+                                    host="192.168.0.91", port=1521, service_name="orclpdb")
+        print("############")
+        print(connection.version)
+
+        cursor = connection.cursor()
+
+        cursor.execute(f"""
+            SELECT SUBSTR(CDPRODUTO, 1, 3) AS "CLASSE",CDPRODUTO, NMPRODUTO, SGUNIDADE FROM PRODUTO
+            WHERE CDPRODUTO = '{code}'
+            
+        """)
+        rows = cursor.fetchall()
+        return rows
+    except oracledb.DatabaseError as e:
+        print (e)
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
---
+++ billings/api/functions.py
@@ -0,0 +1,154 @@
+from ..models import *
+from django.db.models import Sum, Q, Count
+from django.db.models.functions import TruncMonth
+from datetime import datetime, timedelta
+
+import pandas as pd
+import calendar
+import openpyxl
+
+from . import database_query as dq
+
+def CalcServiceTime(today, classification):
+    requests = Requisicao.objects.filter(
+        data_solicitacao__month=today.month,
+        data_solicitacao__year=today.year,
+        finalizada=True,
+        classification=classification
+    )
+    
+    hours = 0
+    if requests.count() > 0:
+        for r in requests:
+            hours += ServiceTime(r.dh_inicio_atendimento, r.dh_finalizada)
+        return (hours / requests.count())
+    else:
+        return 0
+    
+def ServiceTime( start_date, end_date ):
+    
+    start_date_str = datetime.strftime(start_date, "%Y-%m-%d %H:%M:%S%z")
+    end_date_str = datetime.strftime(end_date, "%Y-%m-%d %H:%M:%S%z")
+
+    inicio = datetime.strptime(start_date_str, "%Y-%m-%d %H:%M:%S%z")
+    fim = datetime.strptime(end_date_str, "%Y-%m-%d %H:%M:%S%z") 
+
+    if fim <= inicio:
+        return 0
+
+    horas_uteis = 0
+    data_atual = inicio
+    while data_atual < fim:
+        if data_atual.weekday() < 5: 
+            horas = fim - data_atual
+            horas_uteis += horas.total_seconds() / 3600
+        data_atual += timedelta(days=1)
+
+    return horas_uteis
+
+def LargestRequester(today):
+    try:
+        branchs = Requisicao.objects.filter(
+                data_solicitacao__month=today.month,
+                data_solicitacao__year=today.year,
+            ).values('branch_destino').annotate(
+                total=Count('id')
+            ).order_by('-total')[:5]
+        branchs_ids = [branchs['branch_destino'] for branchs in branchs]
+        return Requisicao.objects.filter(
+                data_solicitacao__month=today.month,
+                data_solicitacao__year=today.year,
+                branch_destino__in=branchs_ids
+            ).values('branch_destino__name').annotate(
+                total=Count('id')
+            ).order_by('-total')
+    except IndexError:
+        return None
+
+def MostRequestedProducts(today):
+    try:
+        return ItensRequisicao.objects.filter(
+            requisicao__data_solicitacao__month=today.month,
+            requisicao__data_solicitacao__year=today.year
+        ).values('produto__name').annotate(
+            total=Count('id')
+        ).order_by('-total')[:5]
+    except IndexError:
+        return None
+    
+def SincRC():
+    CreateProd()
+    rec = dq.consultaRc()
+    print(rec)
+    for r in rec:
+        rc = Requisicao(
+            branch_solicitacao = Branch.objects.get(code=r[0]),
+            branch_destino = Branch.objects.get(code=r[1]),
+            operador = Operador.objects.get(cod=r[4]),
+            dh_aprovacao = r[7],
+            nr_solicitacao = r[2],
+            data_solicitacao = r[3],
+            qtd_itens = r[5],
+            justificativa = r[6],
+        )
+        if not Requisicao.objects.filter(
+                    nr_solicitacao=r[2],
+                    branch_solicitacao = Branch.objects.get(code=r[0]),
+                    branch_destino = Branch.objects.get(code=r[1])).exists():
+            rc.save()
+            print("RC SALVA")
+            itens = dq.consultaItensRc(rc.nr_solicitacao, rc.branch_solicitacao, rc.branch_destino)
+            for i in itens:
+                item = ItensRequisicao(
+                    requisicao = rc,
+                    produto = Produtos.objects.get(code=i[1]),
+                    qtd = i[2],
+                    dt_utiliza = i[3]
+                )
+                item.save()
+                print("Item Atualizado")
+        ClassRequisition()
+
+def ClassRequisition():
+    rc = Requisicao.objects.order_by('-created_at').first()
+    produto = ItensRequisicao.objects.filter(requisicao=rc).first()
+    produto = produto.produto.code[0:3]
+    
+    insumo = [1, 2, 3, 4]
+    epi = 866
+    outros = [5,6,7,8,9]
+    if produto == epi:
+        rc.classification = 2
+        rc.save()
+    elif compare_first_digit(produto,insumo):
+        rc.classification = 1
+        rc.save()
+    else:
+        rc.classification = 3
+        rc.save()
+
+def compare_first_digit(numero, lista_numeros):
+    primeiro_digito_produto = str(numero)[0]
+    for num in lista_numeros:
+        primeiro_digito_num = str(num)[0]
+        if primeiro_digito_produto == primeiro_digito_num:
+            return True
+    return False
+
+def CreateProd():
+    produtos = dq.CriaProdutos()
+    for prod in produtos:
+        p = Produtos(
+            code = prod[1],
+            name = prod[2],
+            classification = ClassProduto.objects.get(code=prod[0]),
+            unidade = prod[3]
+        )
+        try:    
+            if not Produtos.objects.filter(code=prod[1]).exists():
+                p.save()
+                print("Salvo")
+            else:
+                print("Ja Existe")
+        except Exception as e:
+            print(e)
